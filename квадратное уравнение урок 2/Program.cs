1d11b55a754a89a71dace79c566e972cac751911
DeminAS <DeminAS@310D-13>
2022-09-10 12:10:17 +0700

    Добавьте файлы проекта.

---
+++ квадратное уравнение урок 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+Console.WriteLine();
+Double a;
+Double b;
+Double c;
+Double D;
+Double x1;
+Double x2;
+Console.WriteLine("введите а:");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите b:");
+b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите c:");
+c = Convert.ToDouble(Console.ReadLine());
+ D=(b*b) - 4 * a * c;
+if (D >= 0) 
+{
+    x1 = (-b + Math.Sqrt(D)) / (2 * a);
+    x2 = (-b - Math.Sqrt(D)) / (2 * a);
+    Console.WriteLine("x1={0}\n x2={1}", x1, x2);
+    Console.ReadKey(); }
+
+    else
+{ 
+    Console.WriteLine("Нет решений");
+    Console.ReadKey();
+
+}
